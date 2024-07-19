0146142ede189bf393de5e883961d3016249afed
ehdgus128 <ldh159357@naver.com>
2024-07-19 10:28:29 +0900

    KeycloakLogoutController add
    KeycloakLogoutController add

---
+++ master/src/main/java/com/ezace/springkeycloak/controller/KeycloakLogoutController.java
@@ -0,0 +1,35 @@
+package com.ezace.springkeycloak.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+
+@Controller
+@RequestMapping("/keycloak-logout")
+public class KeycloakLogoutController {
+
+    @Value("${keycloak.auth-server-url}")
+    private String keycloakUrl;
+
+    @Value("${keycloak.realm}")
+    private String realm;
+
+    @GetMapping
+    public String logout(HttpServletRequest request, HttpSession session) throws IOException {
+        // 세션에서 토큰 가져오기
+        String idToken = (String) session.getAttribute("idToken");
+
+        // 로그아웃 URL 생성
+        String redirectUri = "http://172.30.1.94:8082/logout";  // 실제 로그아웃 리디렉션 URI로 업데이트
+        String logoutUrl = String.format("%s/protocol/openid-connect/logout?id_token_hint=%s&post_logout_redirect_uri=%s",
+                keycloakUrl, idToken, redirectUri);
+
+        // Keycloak 로그아웃 URL로 리디렉션
+        return "redirect:" + logoutUrl;
+    }
+}
